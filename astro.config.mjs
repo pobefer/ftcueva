99bb9d738cdfe76a6d58a8e6e35e037b15395e7f
pobefer <f.delatorrecueva@gmail.com>
2024-04-04 15:39:04 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pobefer.github.io',
-  base: 'ftcueva',
   integrations: [tailwind()]
 });
