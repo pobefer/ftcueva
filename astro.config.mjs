ce9693b57884323528f3a5e3af4f2cb0b13b10e0
pobefer <f.delatorrecueva@gmail.com>
2024-04-04 23:28:21 +0200

    change all page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pobefer.github.io',
   base: 'ftcueva',
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt()]
 });
